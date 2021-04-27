976397ea014c57bce70d602c1c60e4117181e789
bitbodrit <ya.aibulat1@gmail.com>
2021-04-27 19:45:07 +0300

    redeployApp

--- Commands/RemindDate.cs
+++ Commands/RemindDate.cs
@@ -2,7 +2,6 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
 using ReminderTelegramBot.Model;
 
 namespace ReminderTelegramBot.Commands
@@ -13,22 +12,14 @@ namespace ReminderTelegramBot.Commands
         public override async Task Execute(Message message, ITelegramBotClient client)
         {
             var chatId = message.Chat.Id;
-            var keyBoard = new ReplyKeyboardMarkup
-            {
-                Keyboard = new[]
-                {
-                    new[] { new KeyboardButton("/help") }
-                },
-            };
-
             var reminder = new Reminder();
             var remindTime = reminder.GetRemindTime(message.Text.Remove(Name.Length));
 
-            await client.SendTextMessageAsync(chatId, $"Заказанное время {remindTime}, {reminder.GetTime()}", parseMode: ParseMode.Markdown, replyMarkup: keyBoard);
+            await client.SendTextMessageAsync(chatId, $"Заказанное время {remindTime}, {reminder.GetTime()}", parseMode: ParseMode.Markdown);
             
             if (reminder.CheckTime()) 
             {
-                await client.SendTextMessageAsync(chatId, $"Вовремя {remindTime}, {reminder.GetTime()}", parseMode: ParseMode.Markdown, replyMarkup: keyBoard); 
+                await client.SendTextMessageAsync(chatId, $"Вовремя {remindTime}, {reminder.GetTime()}", parseMode: ParseMode.Markdown); 
             }
         }
     }
