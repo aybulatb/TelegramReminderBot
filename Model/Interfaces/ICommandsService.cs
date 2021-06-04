32d2c4685956a9c6401e4540e9bf6f3584cce2c0
Aibulat <51075780+bitbodrit@users.noreply.github.com>
2021-06-04 11:23:56 +0300

    Update ICommandsService.cs

--- Model/Interfaces/ICommandsService.cs
+++ Model/Interfaces/ICommandsService.cs
@@ -7,7 +7,7 @@ namespace ReminderTelegramBot.Model.Interfaces
     public interface ICommandsService
     {
         /// <summary>
-        /// Возвращает список комманд, которые следуют общему интерфейсу. (Паттерн фабричный метод)
+        /// Возвращает список комманд, которые следуют общему интерфейсу.
         /// </summary>
         /// <returns></returns>
         List<TelegramCommand> GetCommands();
